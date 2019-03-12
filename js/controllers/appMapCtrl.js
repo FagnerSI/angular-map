012e64679abf0c62bc981ef92e0eb1fdfe52bb7f
FagnerSI <fagner314@gmail.com>
2019-03-12 11:15:08 -0300

    função apagar pontos criada

--- js/controllers/appMapCtrl.js
+++ js/controllers/appMapCtrl.js
@@ -3,14 +3,26 @@ angular
     .controller("appMapCtrl", function ($scope, pointsAPI, mapFactory) {
 
         $scope.points = [];
-        $scope.pointList = [];
+        $scope.pointList = [];   
 
-      
 
         $scope.addPoint = function (point) {
             $scope.points.push(point);
             delete $scope.point;
         }
+
+        $scope.deletePoints = function (points) {
+            $scope.points = points.filter(function (point) {
+                if (!point.selected) return point;
+            });
+        }
+
+        $scope.isPointSelected = function (points) {
+            return points.some(function (point) {
+                return point.selected;
+            });
+        };
+
         //Geocodificação
         $scope.sendPoints = function () {           
             $scope.pointList = [];          
