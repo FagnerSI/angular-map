16c2448b2d331b540b846c9d5e7d60d1a1c31ff5
FagnerSI <fagner314@gmail.com>
2019-03-12 08:58:00 -0300

    Dependencias via npm

--- js/controllers/appMapCtrl.js
+++ js/controllers/appMapCtrl.js
@@ -5,11 +5,7 @@ angular
         $scope.points = [];
         $scope.pointList = [];
 
-        //[-3.74166, -38.53532],
-        //[-3.74144, -38.53581],
-        //[-3.74313, -38.53656],
-        //[-3.7383, -38.54225],
-        //[-3.73733, -38.54319],]
+      
 
         $scope.addPoint = function (point) {
             $scope.points.push(point);
